107b7cce5e1ff998076fb01930ffb8ed1c162a29
darwgom7 <darwgom7@gmail.com>
2024-03-04 19:00:34 -0500

    Initial commit

---
+++ src/main/java/com/darwgom/bankinccardapi/BankincCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.darwgom.bankinccardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankincCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankincCardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darwgom/bankinccardapi/BankincCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwgom.bankinccardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankincCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
